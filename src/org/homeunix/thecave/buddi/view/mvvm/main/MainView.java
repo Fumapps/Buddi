16ec3a4dc2908aad1c13fe35150857160429c5d1
Mario Fuksa <44242539+fumapps@users.noreply.github.com>
2025-11-25 21:27:23 +0100

    feat: Implement file open/save/save-as functionality in MainViewModel and add corresponding menu actions and title binding in MainView.

--- src/org/homeunix/thecave/buddi/view/mvvm/main/MainView.java
+++ src/org/homeunix/thecave/buddi/view/mvvm/main/MainView.java
@@ -1,18 +1,21 @@
 package org.homeunix.thecave.buddi.view.mvvm.main;
 
 import javafx.scene.Parent;
-import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.homeunix.thecave.buddi.view.mvvm.View;
 import org.homeunix.thecave.buddi.view.mvvm.myaccounts.MyAccountsView;
 import org.homeunix.thecave.buddi.view.mvvm.mybudget.MyBudgetView;
-
+import org.homeunix.thecave.buddi.view.mvvm.reports.ReportsView;
 import org.homeunix.thecave.buddi.view.mvvm.scheduled.ScheduledTransactionsView;
 
-import org.homeunix.thecave.buddi.view.mvvm.reports.ReportsView;
+import java.io.File;
 
 public class MainView implements View<MainViewModel> {
 
@@ -34,6 +37,25 @@ public class MainView implements View<MainViewModel> {
     }
 
     private void initializeUI() {
+        // Menu Bar
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+
+        MenuItem openItem = new MenuItem("Open...");
+        openItem.setOnAction(e -> handleOpen());
+
+        MenuItem saveItem = new MenuItem("Save");
+        saveItem.setOnAction(e -> handleSave());
+
+        MenuItem saveAsItem = new MenuItem("Save As...");
+        saveAsItem.setOnAction(e -> handleSaveAs());
+
+        fileMenu.getItems().addAll(openItem, saveItem, saveAsItem);
+        menuBar.getMenus().add(fileMenu);
+
+        root.setTop(menuBar);
+
+        // Tabs
         TabPane tabPane = new TabPane();
 
         Tab accountsTab = new Tab("My Accounts");
@@ -57,15 +79,72 @@ public class MainView implements View<MainViewModel> {
         root.setCenter(tabPane);
     }
 
+    private void handleOpen() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Buddi File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Buddi Files", "*.buddi3", "*.buddi"));
+        File file = fileChooser.showOpenDialog(root.getScene().getWindow());
+        if (file != null) {
+            viewModel.load(file);
+        }
+    }
+
+    private void handleSave() {
+        viewModel.save();
+    }
+
+    private void handleSaveAs() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Buddi File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Buddi Files", "*.buddi3"));
+        File file = fileChooser.showSaveDialog(root.getScene().getWindow());
+        if (file != null) {
+            viewModel.saveAs(file);
+        }
+    }
+
     @Override
     public void bind(MainViewModel viewModel) {
         this.viewModel = viewModel;
 
-        // Bind child views
-        myAccountsView.bind(viewModel.getMyAccountsViewModel());
-        myBudgetView.bind(viewModel.getMyBudgetViewModel());
-        scheduledTransactionsView.bind(viewModel.getScheduledTransactionsViewModel());
-        reportsView.bind(viewModel.getReportsViewModel());
+        // Bind title
+        if (root.getScene() != null && root.getScene().getWindow() instanceof Stage) {
+            ((Stage) root.getScene().getWindow()).titleProperty().bind(viewModel.titleProperty());
+        }
+
+        // Bind child views to ViewModel properties
+        viewModel.myAccountsViewModelProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null)
+                myAccountsView.bind(newVal);
+        });
+        // Initial bind
+        if (viewModel.getMyAccountsViewModel() != null) {
+            myAccountsView.bind(viewModel.getMyAccountsViewModel());
+        }
+
+        viewModel.myBudgetViewModelProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null)
+                myBudgetView.bind(newVal);
+        });
+        if (viewModel.getMyBudgetViewModel() != null) {
+            myBudgetView.bind(viewModel.getMyBudgetViewModel());
+        }
+
+        viewModel.scheduledTransactionsViewModelProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null)
+                scheduledTransactionsView.bind(newVal);
+        });
+        if (viewModel.getScheduledTransactionsViewModel() != null) {
+            scheduledTransactionsView.bind(viewModel.getScheduledTransactionsViewModel());
+        }
+
+        viewModel.reportsViewModelProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null)
+                reportsView.bind(newVal);
+        });
+        if (viewModel.getReportsViewModel() != null) {
+            reportsView.bind(viewModel.getReportsViewModel());
+        }
     }
 
     @Override
--- src/org/homeunix/thecave/buddi/view/mvvm/main/MainViewModel.java
+++ src/org/homeunix/thecave/buddi/view/mvvm/main/MainViewModel.java
@@ -1,7 +1,10 @@
 package org.homeunix.thecave.buddi.view.mvvm.main;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import java.io.File;
 import org.homeunix.thecave.buddi.model.Document;
 import org.homeunix.thecave.buddi.model.impl.ModelFactory;
 import org.homeunix.thecave.buddi.view.mvvm.ViewModel;
@@ -10,12 +13,13 @@ import org.homeunix.thecave.buddi.viewmodel.MyAccountsViewModel;
 public class MainViewModel extends ViewModel {
 
     private final StringProperty title = new SimpleStringProperty("Buddi");
-
+    private File currentFile;
     private Document document;
-    private MyAccountsViewModel myAccountsViewModel;
-    private org.homeunix.thecave.buddi.view.mvvm.mybudget.MyBudgetViewModel myBudgetViewModel;
-    private org.homeunix.thecave.buddi.view.mvvm.scheduled.ScheduledTransactionsViewModel scheduledTransactionsViewModel;
-    private org.homeunix.thecave.buddi.view.mvvm.reports.ReportsViewModel reportsViewModel;
+
+    private final ObjectProperty<MyAccountsViewModel> myAccountsViewModel = new SimpleObjectProperty<>();
+    private final ObjectProperty<org.homeunix.thecave.buddi.view.mvvm.mybudget.MyBudgetViewModel> myBudgetViewModel = new SimpleObjectProperty<>();
+    private final ObjectProperty<org.homeunix.thecave.buddi.view.mvvm.scheduled.ScheduledTransactionsViewModel> scheduledTransactionsViewModel = new SimpleObjectProperty<>();
+    private final ObjectProperty<org.homeunix.thecave.buddi.view.mvvm.reports.ReportsViewModel> reportsViewModel = new SimpleObjectProperty<>();
 
     public StringProperty titleProperty() {
         return title;
@@ -29,20 +33,36 @@ public class MainViewModel extends ViewModel {
         this.title.set(title);
     }
 
-    public MyAccountsViewModel getMyAccountsViewModel() {
+    public ObjectProperty<MyAccountsViewModel> myAccountsViewModelProperty() {
         return myAccountsViewModel;
     }
 
-    public org.homeunix.thecave.buddi.view.mvvm.mybudget.MyBudgetViewModel getMyBudgetViewModel() {
+    public MyAccountsViewModel getMyAccountsViewModel() {
+        return myAccountsViewModel.get();
+    }
+
+    public ObjectProperty<org.homeunix.thecave.buddi.view.mvvm.mybudget.MyBudgetViewModel> myBudgetViewModelProperty() {
         return myBudgetViewModel;
     }
 
-    public org.homeunix.thecave.buddi.view.mvvm.scheduled.ScheduledTransactionsViewModel getScheduledTransactionsViewModel() {
+    public org.homeunix.thecave.buddi.view.mvvm.mybudget.MyBudgetViewModel getMyBudgetViewModel() {
+        return myBudgetViewModel.get();
+    }
+
+    public ObjectProperty<org.homeunix.thecave.buddi.view.mvvm.scheduled.ScheduledTransactionsViewModel> scheduledTransactionsViewModelProperty() {
         return scheduledTransactionsViewModel;
     }
 
-    public org.homeunix.thecave.buddi.view.mvvm.reports.ReportsViewModel getReportsViewModel() {
+    public org.homeunix.thecave.buddi.view.mvvm.scheduled.ScheduledTransactionsViewModel getScheduledTransactionsViewModel() {
+        return scheduledTransactionsViewModel.get();
+    }
+
+    public ObjectProperty<org.homeunix.thecave.buddi.view.mvvm.reports.ReportsViewModel> reportsViewModelProperty() {
         return reportsViewModel;
+    }
+
+    public org.homeunix.thecave.buddi.view.mvvm.reports.ReportsViewModel getReportsViewModel() {
+        return reportsViewModel.get();
     }
 
     @Override
@@ -50,15 +70,64 @@ public class MainViewModel extends ViewModel {
         super.initialize();
         try {
             // Load default or autosaved document
-            this.document = ModelFactory.createDocument();
-            this.myAccountsViewModel = new MyAccountsViewModel(document);
-            this.myBudgetViewModel = new org.homeunix.thecave.buddi.view.mvvm.mybudget.MyBudgetViewModel(document);
-            this.scheduledTransactionsViewModel = new org.homeunix.thecave.buddi.view.mvvm.scheduled.ScheduledTransactionsViewModel(
-                    document);
-            this.reportsViewModel = new org.homeunix.thecave.buddi.view.mvvm.reports.ReportsViewModel(document);
+            load(null);
         } catch (Exception e) {
             e.printStackTrace();
             // TODO: Handle error gracefully (show alert)
+        }
+    }
+
+    public void load(File file) {
+        try {
+            if (file == null) {
+                this.document = ModelFactory.createDocument();
+                this.currentFile = null;
+            } else {
+                this.document = ModelFactory.createDocument(file);
+                this.currentFile = file;
+            }
+            updateChildViewModels();
+            updateTitle();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // TODO: Propagate error to view
+        }
+    }
+
+    public void save() {
+        if (currentFile != null) {
+            saveAs(currentFile);
+        } else {
+            // View should handle "Save As" if currentFile is null, but if we get here:
+            // We can't do much without a file.
+            // In a real app, we might throw an exception or return false.
+        }
+    }
+
+    public void saveAs(File file) {
+        try {
+            document.saveAs(file);
+            this.currentFile = file;
+            updateTitle();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // TODO: Propagate error
+        }
+    }
+
+    private void updateChildViewModels() {
+        this.myAccountsViewModel.set(new MyAccountsViewModel(document));
+        this.myBudgetViewModel.set(new org.homeunix.thecave.buddi.view.mvvm.mybudget.MyBudgetViewModel(document));
+        this.scheduledTransactionsViewModel
+                .set(new org.homeunix.thecave.buddi.view.mvvm.scheduled.ScheduledTransactionsViewModel(document));
+        this.reportsViewModel.set(new org.homeunix.thecave.buddi.view.mvvm.reports.ReportsViewModel(document));
+    }
+
+    private void updateTitle() {
+        if (currentFile != null) {
+            setTitle("Buddi - " + currentFile.getName());
+        } else {
+            setTitle("Buddi - [Untitled]");
         }
     }
 }
